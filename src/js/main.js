dd23403b3d3e5db11fee998acd5303f3ea44be22
YaroslavBerkuta <yaroslavberkuta@gmail.com>
2022-06-29 16:47:36 +0300

    pc complate

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,11 @@
 import * as flsFunctions from "./modules/function.js";
-import Swiper, { FreeMode } from "swiper";
+import Swiper, { FreeMode, Navigation, Pagination } from "swiper";
 import { Fancybox } from "@fancyapps/ui";
-import "swiper/css";
+import "swiper/css/bundle";
+import $ from "jquery"
 import "@fancyapps/ui/dist/fancybox.css";
 flsFunctions.isWebp();
-
+Swiper.use([Navigation, Pagination]);
 const brandSwiper = new Swiper(".brand__swiper", {
   slidesPerView: 4,
   loop: true,
@@ -19,5 +20,46 @@ const trainSlider = new Swiper(".tarin__slider", {
     momentum: false,
   },
   modules: [FreeMode],
+  pagination: {
+    el: ".train__control .swiper-pagination",
+    type: "bullets",
+    dynamicBullets: true,
+    dynamicMainBullets: 4,
+  },
+  navigation: {
+    nextEl: ".train__control .swiper-button-next",
+    prevEl: ".train__control .swiper-button-prev",
+  },
 });
 Fancybox.bind('[data-fancybox="gallery"]', {});
+
+const teamSlide = new Swiper(".team", {
+  slidesPerView: 3,
+  loop: true,
+  spaceBetween: 80,
+});
+
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".navigation");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  menu.classList.toggle("active");
+});
+
+
+var $page = $('html, body');
+$('.navigation ul li a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 400);
+    return false;
+});
+
+$('.footer__menu ul li a[href*="#"]').click(function() {
+  $page.animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+  }, 400);
+  return false;
+});
+
